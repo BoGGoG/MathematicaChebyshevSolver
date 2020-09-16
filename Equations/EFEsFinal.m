88a79fc322fb7cd1083f3d41047c955a8ff98a97
mknipfer <marco.knipfer@me.com>
2020-09-16 16:16:34 +0000

    Setup and DEQ1 Coefficients

---
+++ Equations/EFEsFinal.m
@@ -0,0 +1,226 @@
+(* Created with the Wolfram Language for Students - Personal Use Only : www.wolfram.com *)
+{((u^4*(t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/
+       (3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))*
+     (-2/(3*t) + 4*u^3*Bs[t, u] + (2*u*(1 + 2*t*\[Xi][t]))/(3*t^2) - 
+       (2*u^2*(1 + 3*t*\[Xi][t] + 3*t^2*\[Xi][t]^2))/(3*t^3) + 
+       Log[u^(-1)]*((4*u^3*\[ScriptCapitalB]^2)/3 - 
+         (4*u^4*(\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+          (9*t) + (4*u^5*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*
+             \[Xi][t] + 75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(15*t^2)) - 
+       ((u^4*\[ScriptCapitalB]^2)/3 - (4*u^5*(\[ScriptCapitalB]^2 + 
+            15*t*\[ScriptCapitalB]^2*\[Xi][t]))/(45*t) + 
+         (2*u^6*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*\[Xi][t] + 
+            75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(45*t^2))/u + 
+       u^4*Derivative[0, 1][Bs][t, u])^2)/2 + 
+   2*u^3*(-1/(9*t^(5/3)) - t^(1/3)/u^2 + 3*u^2*Ss[t, u] + 
+     (2*u*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+     Log[u^(-1)]*((8*u^3*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+       (4*u^4*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(27*t^(5/3))) - 
+     ((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+       (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))/u + 
+     u^3*Derivative[0, 1][Ss][t, u]) + 
+   u^4*((2*t^(1/3))/u^3 + 6*u*Ss[t, u] + (2*(5 + 9*t*\[Xi][t]))/
+      (81*t^(8/3)) + Log[u^(-1)]*((8*u^2*\[ScriptCapitalB]^2)/(15*t^(2/3)) - 
+       (16*u^3*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(27*t^(5/3))) - 
+     (2*((8*u^3*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^4*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(27*t^(5/3))))/u + 
+     ((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+       (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))/u^2 + 
+     6*u^2*Derivative[0, 1][Ss][t, u] + u^3*Derivative[0, 2][Ss][t, u]))/u^5, 
+ (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + 
+    (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+    Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+      (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^3*
+  (\[ScriptCapitalB]^2/(3*E^(2*((-2*u)/(3*t) + u^4*Bs[t, u] - (2*Log[t])/3 + 
+        (u^2*(1 + 2*t*\[Xi][t]))/(3*t^2) - 
+        (2*u^3*(1 + 3*t*\[Xi][t] + 3*t^2*\[Xi][t]^2))/(9*t^3) + 
+        Log[u^(-1)]*((u^4*\[ScriptCapitalB]^2)/3 - 
+          (4*u^5*(\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+           (45*t) + (2*u^6*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*
+              \[Xi][t] + 75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(45*t^2))))*
+     (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/
+        (3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+       Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+         (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^3) - 
+   2*(t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/
+      (3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+     Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+       (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))) - 
+   u^2*(10/(81*t^(8/3)) - t^(1/3)/u^3 + 2*u*dplusSs[t, u] - 
+     (1 + 3*t*\[Xi][t])/(3*t^(2/3)*u^2) + Log[u^(-1)]*
+      ((-2*t^(1/3)*u*\[ScriptCapitalB]^2)/3 + 
+       (2*u^2*(-2*\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+        (15*t^(2/3)) + (4*u^3*(\[ScriptCapitalB]^2 + 36*t*\[ScriptCapitalB]^2*
+           \[Xi][t] - 135*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/
+        (135*t^(5/3))) - (-(t^(1/3)*u^2*\[ScriptCapitalB]^2)/3 + 
+       (2*u^3*(-2*\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+        (45*t^(2/3)) + (u^4*(\[ScriptCapitalB]^2 + 36*t*\[ScriptCapitalB]^2*
+           \[Xi][t] - 135*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(135*t^(5/3)))/
+      u + u^2*Derivative[0, 1][dplusSs][t, u]) - 
+   (2*u^2*(t^(1/3)/(2*u^2) + (10*u)/(81*t^(8/3)) + u^2*dplusSs[t, u] + 
+      (1 + 3*t*\[Xi][t])/(3*t^(2/3)*u) + 
+      (-1 + 2*t*\[Xi][t] + 3*t^2*\[Xi][t]^2)/(6*t^(5/3)) + 
+      Log[u^(-1)]*(-(t^(1/3)*u^2*\[ScriptCapitalB]^2)/3 + 
+        (2*u^3*(-2*\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+         (45*t^(2/3)) + (u^4*(\[ScriptCapitalB]^2 + 36*t*\[ScriptCapitalB]^2*
+            \[Xi][t] - 135*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/
+         (135*t^(5/3))))*(-1/(9*t^(5/3)) - t^(1/3)/u^2 + 3*u^2*Ss[t, u] + 
+      (2*u*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((8*u^3*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^4*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(27*t^(5/3))) - 
+      ((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))/u + 
+      u^3*Derivative[0, 1][Ss][t, u]))/(t^(1/3)/u - u/(9*t^(5/3)) + 
+     u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + 
+     (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+     Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+       (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))), 
+ (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + 
+    (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+    Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+      (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^4*
+  ((2*\[ScriptCapitalB]^2)/
+    (3*E^(2*((-2*u)/(3*t) + u^4*Bs[t, u] - (2*Log[t])/3 + 
+        (u^2*(1 + 2*t*\[Xi][t]))/(3*t^2) - 
+        (2*u^3*(1 + 3*t*\[Xi][t] + 3*t^2*\[Xi][t]^2))/(9*t^3) + 
+        Log[u^(-1)]*((u^4*\[ScriptCapitalB]^2)/3 - 
+          (4*u^5*(\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+           (45*t) + (2*u^6*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*
+              \[Xi][t] + 75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(45*t^2))))*
+     (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/
+        (3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+       Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+         (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^4) - 
+   (3*u^2*(t^(1/3)/(2*u^2) + (10*u)/(81*t^(8/3)) + u^2*dplusSs[t, u] + 
+      (1 + 3*t*\[Xi][t])/(3*t^(2/3)*u) + 
+      (-1 + 2*t*\[Xi][t] + 3*t^2*\[Xi][t]^2)/(6*t^(5/3)) + 
+      Log[u^(-1)]*(-(t^(1/3)*u^2*\[ScriptCapitalB]^2)/3 + 
+        (2*u^3*(-2*\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+         (45*t^(2/3)) + (u^4*(\[ScriptCapitalB]^2 + 36*t*\[ScriptCapitalB]^2*
+            \[Xi][t] - 135*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/
+         (135*t^(5/3))))*(-2/(3*t) + 4*u^3*Bs[t, u] + 
+      (2*u*(1 + 2*t*\[Xi][t]))/(3*t^2) - 
+      (2*u^2*(1 + 3*t*\[Xi][t] + 3*t^2*\[Xi][t]^2))/(3*t^3) + 
+      Log[u^(-1)]*((4*u^3*\[ScriptCapitalB]^2)/3 - 
+        (4*u^4*(\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+         (9*t) + (4*u^5*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*
+            \[Xi][t] + 75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(15*t^2)) - 
+      ((u^4*\[ScriptCapitalB]^2)/3 - (4*u^5*(\[ScriptCapitalB]^2 + 
+           15*t*\[ScriptCapitalB]^2*\[Xi][t]))/(45*t) + 
+        (2*u^6*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*\[Xi][t] + 
+           75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(45*t^2))/u + 
+      u^4*Derivative[0, 1][Bs][t, u]))/(2*(t^(1/3)/u - u/(9*t^(5/3)) + 
+      u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + 
+      (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))) - 
+   u^2*(1/(3*t^2) + 3*u^2*dplusBs[t, u] + (2*u*(-1 - t*\[Xi][t]))/(3*t^3) + 
+     Log[u^(-1)]*(-2*u^2*\[ScriptCapitalB]^2 + 8*u^3*\[ScriptCapitalB]^2*
+        \[Xi][t] + 5*u^4*((2*\[ScriptCapitalB]^2)/(45*t^2) - 
+         4*\[ScriptCapitalB]^2*\[Xi][t]^2)) - 
+     ((-2*u^3*\[ScriptCapitalB]^2)/3 + 2*u^4*\[ScriptCapitalB]^2*\[Xi][t] + 
+       u^5*((2*\[ScriptCapitalB]^2)/(45*t^2) - 4*\[ScriptCapitalB]^2*
+          \[Xi][t]^2))/u + u^3*Derivative[0, 1][dplusBs][t, u]) - 
+   (3*u^2*(-1/(3*t) + u/(3*t^2) + u^3*dplusBs[t, u] + 
+      (u^2*(-1 - t*\[Xi][t]))/(3*t^3) + Log[u^(-1)]*
+       ((-2*u^3*\[ScriptCapitalB]^2)/3 + 2*u^4*\[ScriptCapitalB]^2*\[Xi][t] + 
+        u^5*((2*\[ScriptCapitalB]^2)/(45*t^2) - 4*\[ScriptCapitalB]^2*
+           \[Xi][t]^2)))*(-1/(9*t^(5/3)) - t^(1/3)/u^2 + 3*u^2*Ss[t, u] + 
+      (2*u*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((8*u^3*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^4*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(27*t^(5/3))) - 
+      ((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))/u + 
+      u^3*Derivative[0, 1][Ss][t, u]))/(2*(t^(1/3)/u - u/(9*t^(5/3)) + 
+      u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + 
+      (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))))), 
+ (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + 
+    (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+    Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+      (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^4*
+  (4 - (10*\[ScriptCapitalB]^2)/
+    (3*E^(2*((-2*u)/(3*t) + u^4*Bs[t, u] - (2*Log[t])/3 + 
+        (u^2*(1 + 2*t*\[Xi][t]))/(3*t^2) - 
+        (2*u^3*(1 + 3*t*\[Xi][t] + 3*t^2*\[Xi][t]^2))/(9*t^3) + 
+        Log[u^(-1)]*((u^4*\[ScriptCapitalB]^2)/3 - 
+          (4*u^5*(\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+           (45*t) + (2*u^6*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*
+              \[Xi][t] + 75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(45*t^2))))*
+     (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/
+        (3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+       Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+         (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^4) + 
+   2*u^3*(-2/u^3 + 2*u*As[t, u] - (2*\[Xi][t])/u^2 + 
+     Log[u^(-1)]*((-4*u*\[ScriptCapitalB]^2)/3 + 4*u^2*\[ScriptCapitalB]^2*
+        \[Xi][t] - 8*u^3*\[ScriptCapitalB]^2*\[Xi][t]^2) - 
+     ((-2*u^2*\[ScriptCapitalB]^2)/3 + (4*u^3*\[ScriptCapitalB]^2*\[Xi][t])/
+        3 - 2*u^4*\[ScriptCapitalB]^2*\[Xi][t]^2)/u + 
+     u^2*Derivative[0, 1][As][t, u]) - 
+   3*u^2*(-1/(3*t) + u/(3*t^2) + u^3*dplusBs[t, u] + 
+     (u^2*(-1 - t*\[Xi][t]))/(3*t^3) + Log[u^(-1)]*
+      ((-2*u^3*\[ScriptCapitalB]^2)/3 + 2*u^4*\[ScriptCapitalB]^2*\[Xi][t] + 
+       u^5*((2*\[ScriptCapitalB]^2)/(45*t^2) - 4*\[ScriptCapitalB]^2*
+          \[Xi][t]^2)))*(-2/(3*t) + 4*u^3*Bs[t, u] + 
+     (2*u*(1 + 2*t*\[Xi][t]))/(3*t^2) - 
+     (2*u^2*(1 + 3*t*\[Xi][t] + 3*t^2*\[Xi][t]^2))/(3*t^3) + 
+     Log[u^(-1)]*((4*u^3*\[ScriptCapitalB]^2)/3 - 
+       (4*u^4*(\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+        (9*t) + (4*u^5*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*
+           \[Xi][t] + 75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(15*t^2)) - 
+     ((u^4*\[ScriptCapitalB]^2)/3 - (4*u^5*(\[ScriptCapitalB]^2 + 
+          15*t*\[ScriptCapitalB]^2*\[Xi][t]))/(45*t) + 
+       (2*u^6*(2*\[ScriptCapitalB]^2 + 10*t*\[ScriptCapitalB]^2*\[Xi][t] + 
+          75*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/(45*t^2))/u + 
+     u^4*Derivative[0, 1][Bs][t, u]) + 
+   (12*u^2*(t^(1/3)/(2*u^2) + (10*u)/(81*t^(8/3)) + u^2*dplusSs[t, u] + 
+      (1 + 3*t*\[Xi][t])/(3*t^(2/3)*u) + 
+      (-1 + 2*t*\[Xi][t] + 3*t^2*\[Xi][t]^2)/(6*t^(5/3)) + 
+      Log[u^(-1)]*(-(t^(1/3)*u^2*\[ScriptCapitalB]^2)/3 + 
+        (2*u^3*(-2*\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+         (45*t^(2/3)) + (u^4*(\[ScriptCapitalB]^2 + 36*t*\[ScriptCapitalB]^2*
+            \[Xi][t] - 135*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/
+         (135*t^(5/3))))*(-1/(9*t^(5/3)) - t^(1/3)/u^2 + 3*u^2*Ss[t, u] + 
+      (2*u*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((8*u^3*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^4*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(27*t^(5/3))) - 
+      ((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3)))/u + 
+      u^3*Derivative[0, 1][Ss][t, u]))/
+    (t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + (1 + 3*t*\[Xi][t])/
+       (3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/(81*t^(8/3)) + 
+      Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))^2 + 
+   u^4*(6/u^4 + 2*As[t, u] + (4*\[Xi][t])/u^3 + 
+     Log[u^(-1)]*((-4*\[ScriptCapitalB]^2)/3 + 8*u*\[ScriptCapitalB]^2*
+        \[Xi][t] - 24*u^2*\[ScriptCapitalB]^2*\[Xi][t]^2) - 
+     (2*((-4*u*\[ScriptCapitalB]^2)/3 + 4*u^2*\[ScriptCapitalB]^2*\[Xi][t] - 
+        8*u^3*\[ScriptCapitalB]^2*\[Xi][t]^2))/u + 
+     ((-2*u^2*\[ScriptCapitalB]^2)/3 + (4*u^3*\[ScriptCapitalB]^2*\[Xi][t])/
+        3 - 2*u^4*\[ScriptCapitalB]^2*\[Xi][t]^2)/u^2 + 
+     4*u*Derivative[0, 1][As][t, u] + u^2*Derivative[0, 2][As][t, u])), 
+ u^3*(dplus2S[t, u] + ((t^(1/3)/u - u/(9*t^(5/3)) + u^3*Ss[t, u] + 
+      (1 + 3*t*\[Xi][t])/(3*t^(2/3)) + (u^2*(5 + 9*t*\[Xi][t]))/
+       (81*t^(8/3)) + Log[u^(-1)]*((2*u^4*\[ScriptCapitalB]^2)/(45*t^(2/3)) - 
+        (4*u^5*\[ScriptCapitalB]^2*(1 + 6*t*\[Xi][t]))/(135*t^(5/3))))*
+     (-1/(3*t) + u/(3*t^2) + u^3*dplusBs[t, u] + (u^2*(-1 - t*\[Xi][t]))/
+        (3*t^3) + Log[u^(-1)]*((-2*u^3*\[ScriptCapitalB]^2)/3 + 
+         2*u^4*\[ScriptCapitalB]^2*\[Xi][t] + 
+         u^5*((2*\[ScriptCapitalB]^2)/(45*t^2) - 4*\[ScriptCapitalB]^2*
+            \[Xi][t]^2)))^2)/2 + 
+   (u^2*(t^(1/3)/(2*u^2) + (10*u)/(81*t^(8/3)) + u^2*dplusSs[t, u] + 
+      (1 + 3*t*\[Xi][t])/(3*t^(2/3)*u) + 
+      (-1 + 2*t*\[Xi][t] + 3*t^2*\[Xi][t]^2)/(6*t^(5/3)) + 
+      Log[u^(-1)]*(-(t^(1/3)*u^2*\[ScriptCapitalB]^2)/3 + 
+        (2*u^3*(-2*\[ScriptCapitalB]^2 + 15*t*\[ScriptCapitalB]^2*\[Xi][t]))/
+         (45*t^(2/3)) + (u^4*(\[ScriptCapitalB]^2 + 36*t*\[ScriptCapitalB]^2*
+            \[Xi][t] - 135*t^2*\[ScriptCapitalB]^2*\[Xi][t]^2))/
+         (135*t^(5/3))))*(-2/u^3 + 2*u*As[t, u] - (2*\[Xi][t])/u^2 + 
+      Log[u^(-1)]*((-4*u*\[ScriptCapitalB]^2)/3 + 4*u^2*\[ScriptCapitalB]^2*
+         \[Xi][t] - 8*u^3*\[ScriptCapitalB]^2*\[Xi][t]^2) - 
+      ((-2*u^2*\[ScriptCapitalB]^2)/3 + (4*u^3*\[ScriptCapitalB]^2*\[Xi][t])/
+         3 - 2*u^4*\[ScriptCapitalB]^2*\[Xi][t]^2)/u + 
+      u^2*Derivative[0, 1][As][t, u]))/2)}
--- source/ChebyshevSolver.wl
+++ source/ChebyshevSolver.wl
@@ -2,3 +2,15 @@ BeginPackage["ChebyshevSolver`"];
 
 Unprotect["ChebyshevSolver`*"];
 ClearAll["ChebyshevSolver`*", "ChebyshevSolver`Private`*"];
+
+Begin["`Private`"];
+
+examplefunction[]:=Print["Hello World!"]
+
+StripArgument[expr_, arg_] := expr /. f_[xx___, arg, yy___] -> f[xx, yy];
+
+Scan[SetAttributes[#, {Protected, ReadProtected}]&,
+     Select[Symbol /@ Names["NumericalPart`*"], Head[#] === Symbol &]];
+
+End[];
+EndPackage[];
